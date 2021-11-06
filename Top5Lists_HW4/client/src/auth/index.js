b564194557d80e8ad21c42bb2085f75b37c55385
Uniiic <chenyouni0306@gmail.com>
2021-11-06 00:07:38 -0400

    first commit

---
+++ Top5Lists_HW4/client/src/auth/index.js
@@ -0,0 +1,185 @@
+import React, { createContext, useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom'
+import api from '../api'
+
+const AuthContext = createContext();
+console.log("create AuthContext: " + AuthContext);
+
+// THESE ARE ALL THE TYPES OF UPDATES TO OUR AUTH STATE THAT CAN BE PROCESSED
+export const AuthActionType = {
+    GET_LOGGED_IN: "GET_LOGGED_IN",
+    REGISTER_USER: "REGISTER_USER",
+    LOGIN_USER: "LOGIN_USER",
+    ERROR_MODAL: "ERROR_MODAL",
+    ERROR_MODAL_INVISIBLE: "ERROR_MODAL_INVISIBLE",
+    LOGOUT_USER: "LOGOUT_USER"
+}
+
+function AuthContextProvider(props) {
+    const [auth, setAuth] = useState({
+        user: null,
+        loggedIn: false,
+        error: false,
+        msg: null
+    });
+    const history = useHistory();
+
+    useEffect(() => {
+        auth.getLoggedIn();
+    }, []);
+
+    const authReducer = (action) => {
+        const { type, payload } = action;
+        switch (type) {
+            case AuthActionType.GET_LOGGED_IN: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: payload.loggedIn,
+                    error: false,
+                    msg: null
+                });
+            }
+            case AuthActionType.REGISTER_USER: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: true,
+                    error: false,
+                    msg: null
+                })
+            }
+            case AuthActionType.LOGIN_USER: {
+                return setAuth({
+                    user: payload.user,
+                    loggedIn: true,
+                    error: false,
+                    msg: null
+                })
+            }
+            case AuthActionType.ERROR_MODAL: {
+                return setAuth({
+                    user: null,
+                    loggedIn: false,
+                    error: payload.error,
+                    msg: payload.msg
+                })
+            }
+            case AuthActionType.ERROR_MODAL_INVISIBLE: {
+                return setAuth({
+                    user: null,
+                    loggedIn: false,
+                    error: payload.error,
+                    msg: payload.msg
+                })
+            }
+            case AuthActionType.LOGOUT_USER: {
+                return setAuth({
+                    user: null,
+                    loggedIn: false,
+                    error: payload.error,
+                    msg: payload.msg
+                })
+            }
+            default:
+                return auth;
+        }
+    }
+
+
+    auth.logoutUser = async function () {
+        const response = await api.logoutUser();
+        authReducer({
+            type: AuthActionType.LOGOUT_USER,
+            payload: {
+                loggedIn: false,
+                    user: null
+            }
+        });
+        
+    }
+
+
+    auth.handleClose = async function () {
+       
+        authReducer({
+            type: AuthActionType.ERROR_MODAL_INVISIBLE,
+            payload: {
+                error: false,
+                msg: null
+            }
+        });
+        
+    }
+
+    auth.getLoggedIn = async function () {
+        const response = await api.getLoggedIn();
+        if (response.status === 200) {
+            authReducer({
+                type: AuthActionType.SET_LOGGED_IN,
+                payload: {
+                    loggedIn: response.data.loggedIn,
+                    user: response.data.user
+                }
+            });
+        }
+        
+    }
+
+    auth.loginUser = async function (userData,store) {
+        try{
+            const response = await api.loginUser(userData);
+            if (response.status === 200) {
+                authReducer({
+                    type: AuthActionType.LOGIN_USER,
+                    payload: {
+                        user: response.data.user
+                    }
+                });
+                history.push("/");
+                store.loadIdNamePairs();
+            }
+        }catch (err){
+            authReducer({
+                type: AuthActionType.ERROR_MODAL,
+                payload: {
+                    error: true,
+                    msg: err.response.data.errorMessage
+                }
+            });
+        }
+    }
+
+    auth.registerUser = async function(userData, store) {
+        try{
+            const response = await api.registerUser(userData);      
+            if (response.status === 200) {
+                authReducer({
+                    type: AuthActionType.REGISTER_USER,
+                    payload: {
+                        user: response.data.user
+                    }
+                })
+                history.push("/");
+                store.loadIdNamePairs();
+            }
+        }catch (err){
+            authReducer({
+                type: AuthActionType.ERROR_MODAL,
+                payload: {
+                    error: true,
+                    msg: err.response.data.errorMessage
+                }
+            });
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            auth
+        }}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthContext;
+export { AuthContextProvider };
---
+++ Top5Lists_HW4/client/src/components/MessageModal.js
@@ -0,0 +1,42 @@
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal'
+import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+
+import { useContext } from 'react';
+import AuthContext from '../auth'
+const style = {
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    p: 2,
+    px: 4,
+    pb: 3,
+};
+const ButtonStyle = {
+    position: "relative",
+    left: 130
+};
+export default function MessageModal(){
+    const { auth } = useContext(AuthContext);
+
+    return(
+        <Modal
+            open={auth.error}
+            onClose={!auth.error}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            style={{display:'flex',alignItems:'center',justifyContent:'center'}}
+        >
+            <Box sx={style}>
+                <Alert severity="warning">{auth.msg}</Alert>
+                <Button style={ButtonStyle}
+                    onClick={auth.handleClose}
+                >
+                    Got it!
+                </Button>
+            </Box>
+
+        </Modal>
+    )
+}
